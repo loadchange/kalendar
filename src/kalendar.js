a6ff9dc0b24a9857f4141fe6c9c0e37ac492c9b2
王彦民 <soocto@gmail.com>
2018-08-30 16:44:03 +0800

    refactor: Optimize the refactoring

--- src/kalendar.js
+++ src/kalendar.js
@@ -2,91 +2,66 @@ import Day from './day'
 import * as utils from './utils'
 
 export default class Kalendar {
-    constructor(options = {}) {
-        this.startTime = options.startTime
-        this.endTime = options.endTime
-        this.unifiedMount = options.unifiedMount || {}
-        this.mount = options.mount || {}
-        this.weekStart = options.weekStart || 0
+    constructor({start, end, unifiedMount = {}, mount = {}, weekStart = 0}) {
+        this.startTime = start
+        this.endTime = end
+        this.unifiedMount = unifiedMount
+        this.mount = mount
+        this.weekStart = weekStart
         return this._create()
     }
 
     get startDate() {
-        return this.startTime ? this._getDate(this.startTime) : new Date()
+        return this.startTime ? utils.getDate(this.startTime) : new Date()
     }
 
     get endDate() {
-        if (this.endTime) {
-            return this._getDate(this.endTime)
-        }
-        let date = this.startDate
+        if (this.endTime) return utils.getDate(this.endTime)
+        const date = this.startDate
         date.setMonth(date.getMonth() + 3)
         return date
     }
 
-    _getMonthDays(date) {
-        const year = date.getFullYear()
-        const month = date.getMonth()
-        const leapYear = !(year % 4) && ((year % 100) || !(year % 400))
-        const days = [31, leapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        return days[month]
-    }
-
-    _getDate(dateStr) {
-        let date = new Date()
-        let year, month
-        [year, month] = dateStr.split('-')
-        date.setFullYear(+year)
-        date.setMonth(--month)
-        return date
-    }
-
     _create() {
-        let table = {}
+        const {mount, weekStart, unifiedMount} = this
+        const table = {}
         let count = (this.endDate.getFullYear() * 12 + this.endDate.getMonth() + 1)
             - (this.startDate.getFullYear() * 12 + this.startDate.getMonth() + 1)
-        if (count < 0) {
-            return null
-        }
+        if (count < 0) return null
         let idx = 0
         do {
-            let date = this.startDate
+            const date = this.startDate
             date.setMonth(date.getMonth() + idx)
-            let monthTable = []
-            let days = this._getMonthDays(date)
-            date.setDate(1)
-            let day = date.getDay()
-            let skip = 0
-            // 月的第一天不是起点
-            if (day !== this.weekStart) {
-                skip = day - this.weekStart
-            }
-            for (let i = 0; i < days + skip; i += 7) {
-                let week = []
-                let num = 7
-                if (!i && skip) {
-                    for (let k = 0; k < skip; k++) {
-                        week.push(null)
-                    }
-                    num -= skip
-                }
-                for (let j = 0; j < num; j++) {
-                    let dateText = utils.getChinaStandard(date)
-                    let mount = Object.assign({}, this.unifiedMount, this.mount[dateText])
-                    week.push(new Day(date, mount))
-                    if (date.getDate() < days) {
-                        date.setDate(date.getDate() + 1)
-                    } else {
-                        break
-                    }
-                }
-                monthTable.push(week)
-            }
-
+            const monthTable = Kalendar.monthly({date, mount, weekStart, unifiedMount})
             table[utils.getChinaStandard(date, true)] = monthTable
             count--
             idx++
         } while (count > 0)
         return table
+    }
+
+    static monthly({date, mount, weekStart = 0, unifiedMount}) {
+        const monthTable = []
+        const days = utils.getMonthDays(date)
+        date.setDate(1)
+        const day = date.getDay()
+        let skip = 0
+        if (day !== weekStart) skip = day - weekStart
+        for (let i = 0; i < days + skip; i += 7) {
+            const week = []
+            let num = 7
+            if (!i && skip) {
+                for (let k = 0; k < skip; k++) week.push(null)
+                num -= skip
+            }
+            for (let j = 0; j < num; j++) {
+                const dateText = utils.getChinaStandard(date)
+                week.push(new Day(date, Object.assign(unifiedMount, mount[dateText])))
+                if (date.getDate() >= days) break
+                date.setDate(date.getDate() + 1)
+            }
+            monthTable.push(week)
+        }
+        return monthTable
     }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,24 @@
 export function getChinaStandard(date, streamline) {
-    let month = date.getMonth()
+    let month = date.getMonth() + 1
     let day = date.getDate()
-    if (month < 10) {
-        month = `0${month + 1}`
-    }
-    if (day < 10) {
-        day = `0${day}`
-    }
-    let arr = streamline ? [date.getFullYear(), month] : [date.getFullYear(), month, day]
+    month = `${month < 10 ? '0' : ''}${month}`
+    day = `${day < 10 ? '0' : ''}${day}`
+    const arr = streamline ? [date.getFullYear(), month] : [date.getFullYear(), month, day]
     return arr.join('-')
+}
+
+export function getMonthDays(date) {
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const leapYear = !(year % 4) && ((year % 100) || !(year % 400))
+    const days = [31, leapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    return days[month]
+}
+
+export function getDate(dateStr) {
+    const date = new Date()
+    const [year, month] = dateStr.split('-')
+    date.setFullYear(+year)
+    date.setMonth(month - 1)
+    return date
 }
