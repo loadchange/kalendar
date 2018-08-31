2411d689334bea41328e882bc82bad08898df0f4
王彦民 <soocto@gmail.com>
2018-08-31 15:56:36 +0800

    refactor: Building code

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,12 +1,24 @@
-import rollup from 'rollup'
-import babel from 'rollup-plugin-babel'
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import {uglify} from 'rollup-plugin-uglify';
 
 export default {
-    input: 'src/kalendar.js',
-    plugins: [babel()],
+    entry: 'src/kalendar.js',
     output: {
         name: 'Kalendar',
         file: 'dist/kalendar.js',
-        format: 'umd'
-    }
-}
+        format: 'umd',
+        sourcemap: false
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        babel(),
+        replace({
+            ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+        }),
+        (process.env.NODE_ENV === 'production' && uglify()),
+    ],
+};
