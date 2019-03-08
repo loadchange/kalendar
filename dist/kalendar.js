77988713462b0ada8bdb0590884d29e2162febd4
LoadChange <soocto@gmail.com>
2019-03-08 20:51:07 +0800

    fix: eslint

--- dist/kalendar.js
+++ dist/kalendar.js
@@ -98,6 +98,145 @@
     throw new TypeError("Invalid attempt to destructure non-iterable instance");
   }
 
+  var animals = ['猴', '鸡', '狗', '猪', '鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊'];
+  var lunarInfo = [0x04bd8, 0x04ae0, 0x0a570, 0x054d5, 0x0d260, 0x0d950, 0x16554, 0x056a0, 0x09ad0, 0x055d2, // 1900-1909
+  0x04ae0, 0x0a5b6, 0x0a4d0, 0x0d250, 0x1d255, 0x0b540, 0x0d6a0, 0x0ada2, 0x095b0, 0x14977, // 1910-1919
+  0x04970, 0x0a4b0, 0x0b4b5, 0x06a50, 0x06d40, 0x1ab54, 0x02b60, 0x09570, 0x052f2, 0x04970, // 1920-1929
+  0x06566, 0x0d4a0, 0x0ea50, 0x06e95, 0x05ad0, 0x02b60, 0x186e3, 0x092e0, 0x1c8d7, 0x0c950, // 1930-1939
+  0x0d4a0, 0x1d8a6, 0x0b550, 0x056a0, 0x1a5b4, 0x025d0, 0x092d0, 0x0d2b2, 0x0a950, 0x0b557, // 1940-1949
+  0x06ca0, 0x0b550, 0x15355, 0x04da0, 0x0a5b0, 0x14573, 0x052b0, 0x0a9a8, 0x0e950, 0x06aa0, // 1950-1959
+  0x0aea6, 0x0ab50, 0x04b60, 0x0aae4, 0x0a570, 0x05260, 0x0f263, 0x0d950, 0x05b57, 0x056a0, // 1960-1969
+  0x096d0, 0x04dd5, 0x04ad0, 0x0a4d0, 0x0d4d4, 0x0d250, 0x0d558, 0x0b540, 0x0b6a0, 0x195a6, // 1970-1979
+  0x095b0, 0x049b0, 0x0a974, 0x0a4b0, 0x0b27a, 0x06a50, 0x06d40, 0x0af46, 0x0ab60, 0x09570, // 1980-1989
+  0x04af5, 0x04970, 0x064b0, 0x074a3, 0x0ea50, 0x06b58, 0x05ac0, 0x0ab60, 0x096d5, 0x092e0, // 1990-1999
+  0x0c960, 0x0d954, 0x0d4a0, 0x0da50, 0x07552, 0x056a0, 0x0abb7, 0x025d0, 0x092d0, 0x0cab5, // 2000-2009
+  0x0a950, 0x0b4a0, 0x0baa4, 0x0ad50, 0x055d9, 0x04ba0, 0x0a5b0, 0x15176, 0x052b0, 0x0a930, // 2010-2019
+  0x07954, 0x06aa0, 0x0ad50, 0x05b52, 0x04b60, 0x0a6e6, 0x0a4e0, 0x0d260, 0x0ea65, 0x0d530, // 2020-2029
+  0x05aa0, 0x076a3, 0x096d0, 0x04afb, 0x04ad0, 0x0a4d0, 0x1d0b6, 0x0d250, 0x0d520, 0x0dd45, // 2030-2039
+  0x0b5a0, 0x056d0, 0x055b2, 0x049b0, 0x0a577, 0x0a4b0, 0x0aa50, 0x1b255, 0x06d20, 0x0ada0, // 2040-2049
+  0x14b63, 0x09370, 0x049f8, 0x04970, 0x064b0, 0x168a6, 0x0ea50, 0x06b20, 0x1a6c4, 0x0aae0, // 2050-2059
+  0x092e0, 0x0d2e3, 0x0c960, 0x0d557, 0x0d4a0, 0x0da50, 0x05d55, 0x056a0, 0x0a6d0, 0x055d4, // 2060-2069
+  0x052d0, 0x0a9b8, 0x0a950, 0x0b4a0, 0x0b6a6, 0x0ad50, 0x055a0, 0x0aba4, 0x0a5b0, 0x052b0, // 2070-2079
+  0x0b273, 0x06930, 0x07337, 0x06aa0, 0x0ad50, 0x14b55, 0x04b60, 0x0a570, 0x054e4, 0x0d160, // 2080-2089
+  0x0e968, 0x0d520, 0x0daa0, 0x16aa6, 0x056d0, 0x04ae0, 0x0a9d4, 0x0a4d0, 0x0d150, 0x0f252, // 2090-2099
+  0x0d520];
+
+  var Solar2lunar =
+  /*#__PURE__*/
+  function () {
+    function Solar2lunar(date) {
+      _classCallCheck(this, Solar2lunar);
+
+      this.y = date.getFullYear();
+      this.m = date.getMonth();
+      this.d = date.getDate();
+    }
+    /**
+     * 返回农历y年闰月是哪个月；若y年没有闰月 则返回0
+     * @param y
+     * @returns {number}
+     */
+
+
+    _createClass(Solar2lunar, [{
+      key: "leapMonth",
+      value: function leapMonth(y) {
+        return lunarInfo[y - 1900] & 0xf;
+      }
+    }, {
+      key: "leapDays",
+      value: function leapDays(y) {
+        if (this.leapMonth(y)) {
+          return lunarInfo[y - 1900] & 0x10000 ? 30 : 29;
+        }
+
+        return 0;
+      }
+    }, {
+      key: "lYearDays",
+      value: function lYearDays(y) {
+        var i;
+        var sum = 348;
+
+        for (i = 0x8000; i > 0x8; i >>= 1) {
+          sum += lunarInfo[y - 1900] & i ? 1 : 0;
+        }
+
+        return sum + this.leapDays(y);
+      }
+    }, {
+      key: "monthDays",
+      value: function monthDays(y, m) {
+        return lunarInfo[y - 1900] & 0x10000 >> m ? 30 : 29;
+      }
+    }, {
+      key: "convert",
+      value: function convert() {
+        var y = this.y,
+            m = this.m,
+            d = this.d;
+        if (y < 1900 || y > 2100 || y === 1900 && !m && d < 31) return {};
+        var i;
+        var temp = 0;
+        var offset = (Date.UTC(y, m, d) - Date.UTC(1900, 0, 31)) / 86400000;
+
+        for (i = 1900; i < 2101 && offset > 0; i++) {
+          temp = this.lYearDays(i);
+          offset -= temp;
+        }
+
+        if (offset < 0) {
+          offset += temp;
+          i -= 1;
+        }
+
+        var year = i; // 农历年
+
+        var leap = this.leapMonth(i); // 闰哪个月
+
+        var isLeap = false;
+
+        for (i = 1; i < 13 && offset > 0; i++) {
+          if (leap > 0 && i === leap + 1 && !isLeap) {
+            // 闰月
+            i -= 1;
+            isLeap = true;
+            temp = this.leapDays(year); // 计算农历闰月天数
+          } else {
+            temp = this.monthDays(year, i); // 计算农历普通月天数
+          }
+
+          isLeap = !(isLeap && i === leap + 1); // 解除闰月
+
+          offset -= temp;
+        }
+
+        if (!offset && leap > 0 && i === leap + 1) {
+          isLeap = !isLeap;
+          if (isLeap) i -= 1;
+        }
+
+        if (offset < 0) {
+          offset += temp;
+          i -= 1;
+        }
+
+        var month = i; // 农历月
+
+        var day = offset + 1; // 农历日
+
+        return {
+          year: year,
+          leap: leap,
+          month: month,
+          day: day
+        };
+      }
+    }]);
+
+    return Solar2lunar;
+  }();
+
   var getChinaStandard = function getChinaStandard(date, streamline) {
     var month = date.getMonth() + 1;
     var day = date.getDate();
@@ -136,9 +275,11 @@
   var calcMonth = function calcMonth(date) {
     return date.getFullYear() * 12 + date.getMonth() + 1;
   };
-  var animals = ['猴', '鸡', '狗', '猪', '鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊'];
   var getAnimal = function getAnimal(date) {
     return animals[date.getFullYear() % 12];
+  };
+  var lunar = function lunar(date) {
+    return new Solar2lunar(date).convert();
   };
 
   var Day =
@@ -163,6 +304,7 @@
         past: toDay > timestamp,
         today: toDay === eraseTime(timestamp),
         animal: getAnimal(date),
+        lunar: lunar(date),
         dateText: dateText,
         timestamp: timestamp
       }, extension);
--- src/solar2lunar.js
+++ src/solar2lunar.js
@@ -1,8 +1,6 @@
-import { lunarInfo } from './constants'
-
+import { lunarInfo } from './constants';
 
 class Solar2lunar {
-
   constructor(date) {
     this.y = date.getFullYear();
     this.m = date.getMonth();
@@ -14,7 +12,7 @@ class Solar2lunar {
    * @param y
    * @returns {number}
    */
-  leapMonth(y) { //闰字编码 \u95f0
+  leapMonth(y) {
     return (lunarInfo[y - 1900] & 0xf);
   }
 
@@ -26,7 +24,8 @@ class Solar2lunar {
   }
 
   lYearDays(y) {
-    let i, sum = 348;
+    let i;
+    let sum = 348;
     for (i = 0x8000; i > 0x8; i >>= 1) {
       sum += (lunarInfo[y - 1900] & i) ? 1 : 0;
     }
@@ -41,7 +40,8 @@ class Solar2lunar {
     const { y, m, d } = this;
     if (y < 1900 || y > 2100 || (y === 1900 && !m && d < 31)) return {};
 
-    let i, temp = 0;
+    let i;
+    let temp = 0;
     let offset = (Date.UTC(y, m, d) - Date.UTC(1900, 0, 31)) / 86400000;
     for (i = 1900; i < 2101 && offset > 0; i++) {
       temp = this.lYearDays(i);
@@ -49,20 +49,19 @@ class Solar2lunar {
     }
     if (offset < 0) {
       offset += temp;
-      i--;
+      i -= 1;
     }
 
     const year = i; // 农历年
-    const leap = this.leapMonth(i); //闰哪个月
+    const leap = this.leapMonth(i); // 闰哪个月
 
     let isLeap = false;
     for (i = 1; i < 13 && offset > 0; i++) {
       if (leap > 0 && i === (leap + 1) && !isLeap) { // 闰月
-        --i;
+        i -= 1;
         isLeap = true;
         temp = this.leapDays(year); // 计算农历闰月天数
-      }
-      else {
+      } else {
         temp = this.monthDays(year, i);// 计算农历普通月天数
       }
       isLeap = !(isLeap && i === (leap + 1)); // 解除闰月
@@ -71,13 +70,13 @@ class Solar2lunar {
 
     if (!offset && leap > 0 && i === leap + 1) {
       isLeap = !isLeap;
-      if (isLeap) --i;
+      if (isLeap) i -= 1;
     }
     if (offset < 0) {
       offset += temp;
-      --i;
+      i -= 1;
     }
-    const month = i; //农历月
+    const month = i; // 农历月
     const day = offset + 1; // 农历日
 
     return { year, leap, month, day };
